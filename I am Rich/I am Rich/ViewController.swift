dcf9ab7192ac02161bfca0f1c2d465614526a847
Sabyrzhan Tynybayev <stynybayev@twilio.com>
2020-05-20 23:47:39 +0300

    Initial commit

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Sabyrzhan Tynybayev on 18.05.2020.
+//  Copyright © 2020 Sabyrzhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
