9d4f37dd72a7a30a9ab88318e27ecde84c52492a
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2020-05-21 04:57:16 +0300

    Add README.md, complete Chapter 3

---
+++ I am Poor/I am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Sabyrzhan Tynybayev on 5/21/20.
+//  Copyright © 2020 Sabyrzhan Tynybayev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,7 @@
+# My "Udemy's iOS 13 and Swift 5 Complete bootcamp" learning progress
+
+This is the repository which contains my progress while learning every chapter from Udemy's course.
+
+Currently I have completed following chapters:
+* Chapter 2: Building "I am Rich" application
+* Chapter 3: Something like homework: build "I am Poor" application similar to "I am Rich" app
